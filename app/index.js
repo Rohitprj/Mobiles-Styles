fae856ba30cd114c5cb81841e76c5212348ddcd9
ASHA LEARNOLOGY <ashalearnology.inst@gmail.com>
2024-05-16 17:52:34 +0530

    changed

---
+++ app/index.js
@@ -0,0 +1,159 @@
+import {View} from "react-native"
+
+export default function App(){
+    return(
+       <View
+       style={{
+        padding: 10,
+        margin: 30,
+        backgroundColor: "red",
+        height: 300,
+        width: 300,
+        display:"flex",
+        flexDirection:"row",
+        flexWrap:"wrap",
+        gap: 20
+       }}
+       >
+        <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+
+       </View>
+
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+
+       </View>
+
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+
+       </View>
+
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+
+       </View>
+       
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+
+       </View>
+       
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+
+       </View>
+       
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+
+       </View>
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+        
+       </View>
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+        
+       </View>
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+        
+       </View>
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+        
+       </View>
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+        
+       </View>
+       <View
+       style={{
+        backgroundColor: "blue",
+        height: 30,
+        width: 30,
+        borderWidth:2
+       }}
+       >
+        
+       </View>
+
+       </View>
+    )
+
+    
+}
